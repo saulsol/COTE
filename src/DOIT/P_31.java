1868a7ea3debc092d102aac11dfefa1e024529ce
saul <dlathf3210@naver.com>
2023-03-03 15:36:43 +0900

    03/03

--- src/DOIT/P_31.java
+++ src/DOIT/P_31.java
@@ -2,12 +2,26 @@ package DOIT;
 
 // 숫자의 합 구하기
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P_31 {
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) {
+        // 숫자의 합 구하기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
+        String s = br.readLine();
 
+        char [] cNum = s.toCharArray();
 
+        int sum = 0;
+        for(int i=0; i<cNum.length; i++){
+            sum += cNum[i] - '0';
+        }
+
+        System.out.println(sum);
 
 
     }
