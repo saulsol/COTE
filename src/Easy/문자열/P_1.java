f8685c9cb43e4f67b8fdc9483063e87ee738e0b5
saul <dlathf3210@naver.com>
2023-04-18 04:45:52 +0900

    04/18

---
+++ src/Easy/문자열/P_1.java
@@ -0,0 +1,28 @@
+package Easy.문자열;
+
+import java.util.Scanner;
+
+// 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내보자 -> 대소문자 구분 X
+public class P_1 {
+
+    public static int solution(String str, char t){
+        int answer = 0;
+        str = str.toUpperCase(); // --> 전부 대문자로 변환
+        t = Character.toUpperCase(t); // 기준 문자도 대문자로 변환
+
+        for(int i=0; i<str.length(); i++){
+          if(str.charAt(i) == t) answer ++; // 문자열 돌아가면서 인덱싱
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next(); // 문자열 하나를 읽어들이는 메서드
+        char c = sc.next().charAt(0); // 0번째 문자 갖고와라
+        System.out.println(P_1.solution(str, c));
+
+    }
+
+}
--- src/Easy/문자열/문자찾기.java
+++
@@ -1,4 +0,0 @@
-package Easy.문자열;
-
-public class 문자찾기 {
-}
