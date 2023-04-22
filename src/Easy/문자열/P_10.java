9a3075da8a6939d16afd527f7a4b8c5b292a8c9a
saul <dlathf3210@naver.com>
2023-04-22 20:32:34 +0900

    04/22

---
+++ src/Easy/문자열/P_10.java
@@ -0,0 +1,21 @@
+package Easy.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P_10 {
+    private static int solution(String s){
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(solution(s));
+
+    }
+}
