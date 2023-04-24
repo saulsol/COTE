3a895e857fc6b6d728431789f2c1cd4c6b6023ec
saul <dlathf3210@naver.com>
2023-04-24 17:43:23 +0900

    04/24

--- src/Easy/문자열/P_10.java
+++ src/Easy/문자열/P_10.java
@@ -3,10 +3,48 @@ package Easy.문자열;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+// 한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램 작성
 public class P_10 {
-    private static int solution(String s){
-        int answer = 0;
+    private static String solution(String s, char t){
+        String answer = "";
+        char [] ca = s.toCharArray();
+        int count = 0;
+        int [] ia = new int[ca.length];
+
+        // 순행
+        for(int i=0; i<ia.length; i++){
+            if(ca[i] == t){
+                count = 0;
+                ia[i] = count;
+            }
+            ia[i] = ++count;
+        }
+
+        count = 0;
+
+        // 역순
+        for(int i=ia.length-1; i>=0; i--){
+            if(ca[i] == t){
+                count = 0;
+                ia[i] = count;
+            }
+
+            count++;
+
+            if(ia[i]>count){
+                ia[i] = count;
+            }
+
+        }
+
+
+        // 출력
+        for(int i=0; i<ia.length; i++ ){
+            answer = answer + ia[i] + " ";
+        }
+        
 
         return answer;
     }
@@ -14,8 +52,12 @@ public class P_10 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        System.out.println(solution(s));
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
+
+        String s = stringTokenizer.nextToken();
+        char target = stringTokenizer.nextToken().charAt(0);
+
+        System.out.println(solution(s, target));
 
     }
 }
