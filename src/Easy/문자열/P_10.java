799099ea85dfcfec95a8610c07924a2c3f32ded4
saul <dlathf3210@naver.com>
2023-05-04 06:52:34 +0900

    05/04

--- src/Easy/문자열/P_10.java
+++ src/Easy/문자열/P_10.java
@@ -3,48 +3,51 @@ package Easy.문자열;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // 한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램 작성
 public class P_10 {
-    private static String solution(String s, char t){
-        String answer = "";
-        char [] ca = s.toCharArray();
-        int count = 0;
-        int [] ia = new int[ca.length];
+    private static int [] solution(String s, char t){
+        char[] ac = s.toCharArray();
+        int [] leftTarget = new int [ac.length];
+        int [] rightTarget = new int [ac.length];
+        int [] answer = new int[ac.length];
+        int p = 100;
 
-        // 순행
-        for(int i=0; i<ia.length; i++){
-            if(ca[i] == t){
-                count = 0;
-                ia[i] = count;
+        for (int i=0; i<ac.length; i++){ // 자신의 왼쪽 기준에서의 타겟에 대한 증가
+            p++;
+            if(ac[i] == t){
+                p = 0;
+                leftTarget[i] = p;
             }
-            ia[i] = ++count;
+            leftTarget[i] = p;
         }
 
-        count = 0;
+        p = 100; // p 초기화
 
-        // 역순
-        for(int i=ia.length-1; i>=0; i--){
-            if(ca[i] == t){
-                count = 0;
-                ia[i] = count;
+        for(int j=ac.length-1; j>=0; j--){ // 자신의 오른쪽 기준에서의 타겟에 대한 증가
+            p++;
+            if(ac[j] == t){
+                p = 0;
+                rightTarget[j] = p;
             }
 
-            count++;
+            rightTarget[j] = p;
+        }
 
-            if(ia[i]>count){
-                ia[i] = count;
+        for(int k=0; k<ac.length; k++){
+
+            if(leftTarget[k] < rightTarget[k]){
+                answer[k] = leftTarget[k];
+            }else {
+                answer[k] = rightTarget[k];
             }
 
         }
 
 
-        // 출력
-        for(int i=0; i<ia.length; i++ ){
-            answer = answer + ia[i] + " ";
-        }
-        
+
 
         return answer;
     }
@@ -57,7 +60,7 @@ public class P_10 {
         String s = stringTokenizer.nextToken();
         char target = stringTokenizer.nextToken().charAt(0);
 
-        System.out.println(solution(s, target));
+        System.out.println(Arrays.toString(solution(s, target)));
 
     }
 }
