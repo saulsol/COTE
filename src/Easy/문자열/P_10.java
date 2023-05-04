813523a2b62e9f78ddc3bdb49db740e5f5488092
saul <dlathf3210@naver.com>
2023-05-04 11:16:31 +0900

    05/04

--- src/Easy/문자열/P_10.java
+++ src/Easy/문자열/P_10.java
@@ -46,11 +46,38 @@ public class P_10 {
 
         }
 
+        return answer;
+    }
 
+    private int [] solution2(String s, char t){
+        int[] answer = new int[s.length()];
+        int p=1000;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == t){
+                p = 0;
+                answer[i] = p;
+            }else{
+                p++;
+                answer[i] = p;
+            }
+        }
+        p = 1000; // p 초기화 해야한다
 
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i) == t) p=0;
+            else{
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
 
         return answer;
     }
+
+
+
+
+
 
     public static void main(String[] args) throws IOException {
 
