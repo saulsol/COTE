d1fa883179e030935b2f03d506aca2935e043e57
saul <dlathf3210@naver.com>
2023-07-12 09:03:51 +0900

    07/12

---
+++ src/Easy/정렬/BinaryHeaps.java
@@ -0,0 +1,35 @@
+package Easy.정렬;
+
+// 힙이란?
+// 최대값이나 최소값을 찾아내는 연산을 빠르게 하기위해 고안된 연산 방법
+
+// 최소 힙
+// 작은 값을 항상 루트에 오게 한다.
+// 부모 노드가 자식 노드보다 작은 값
+
+// 최대 힙
+// 가장 큰 값이 항상 루트에 오게 한다.
+// 부모 노드가 자식 노드보다 큰 값
+
+// 병합 정렬보단 아니다.
+public class BinaryHeaps {
+
+    static void swap(int [] a, int idx, int idx2){
+        int temp = a[idx];
+        a[idx] = a[idx2];
+        a[idx2] = temp;
+    }
+
+    static void downHeap(int [] a, int left, int right){
+
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
