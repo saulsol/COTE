0166ca8cef680a359efbde523a180f20e4cebb04
saul <dlathf3210@naver.com>
2023-07-22 15:45:58 +0900

    0722

---
+++ src/DOIT_ALCOTE_BJ/P_11659.java
@@ -0,0 +1,16 @@
+package DOIT_ALCOTE_BJ;
+// 구간합 구하기
+// 수 N개가 주어졌을 때 i번째 수에서 j번째 수까지의 합을 출력한다.
+// 5 3
+// 5 4 3 2 1
+// 1 3 -> 5 + 4 + 3 = 12
+// 2 4 -> 4 + 3 + 2
+// 5 5 -> 1
+
+
+public class P_11659 {
+    public static void main(String[] args) {
+
+    }
+
+}
