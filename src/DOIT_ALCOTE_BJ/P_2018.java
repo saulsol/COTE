f0941552c4a3a826a3bb2bc45f8b1e5d63868352
saul <dlathf3210@naver.com>
2023-07-25 19:23:41 +0900

    0725

---
+++ src/DOIT_ALCOTE_BJ/P_2018.java
@@ -0,0 +1,16 @@
+package DOIT_ALCOTE_BJ;
+
+// 어떠한 자연수 N은 몇 개의 연속된 자연수의 합으로 나타낼 수 있다.
+// 15를 나타내는 방법은 7 + 8 / 4 + 5 + 6 / 1 + 2 + 3 + 4 + 5
+
+public class P_2018 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+
+}
---
+++ src/Easy/슬라이딩_투포인터/P_5.java
@@ -0,0 +1,48 @@
+package Easy.슬라이딩_투포인터;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 연속된 자연수의 합
+// 7 + 8 = 15
+// 4 + 5 + 6 = 15
+// 1 + 2 + 3 + 4 + 5 = 15
+public class P_5 {
+
+    private static int solution(int N){
+        int sum = 0;
+        int answer = 0;
+        int lt = 0;
+        int m = N / 2 + 1; // 몫 보다 1 더 크다
+
+        int [] arr = new int[m];
+
+        for(int i=0; i<m; i++)
+            arr[i] = i+1; // 0부터 1씩
+
+        for(int rt = 0; rt<m; rt ++){
+
+            sum = sum + arr[rt];
+            if(sum == N) answer ++;
+
+            while(sum >= N) {
+                sum = sum - arr[lt++];
+                if(sum == N) answer++;
+            }
+
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int a = solution(n);
+        System.out.println(a);
+    }
+}
