49c5fd3d962c63fa3b519217d3c4418de554166c
saul <dlathf3210@naver.com>
2023-07-31 09:02:30 +0900

    0731

---
+++ src/DOIT_ALCOTE_BJ/P_12891.java
@@ -0,0 +1,11 @@
+package DOIT_ALCOTE_BJ;
+// 슬라이딩 윈도우
+// 슬라이딩 윈도우는 2개의 포인터로 범위를 지정한 다음 범위(window)를 유지한 채로 이동(sliding)하여 문제를 해결힌다
+// 투 포인터 와 비슷
+
+// DNA 비밀번호
+public class P_12891 {
+    public static void main(String[] args) {
+
+    }
+}
