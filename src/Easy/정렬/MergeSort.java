3f747b75ea83ff78e2cf6fa034547966e4dc9011
saul <dlathf3210@naver.com>
2024-01-14 01:20:58 +0900

    01/14

--- src/Easy/정렬/MergeSort.java
+++ src/Easy/정렬/MergeSort.java
@@ -1,43 +1,61 @@
 package Easy.정렬;
 
 public class MergeSort {
-    public static int[] buff;                    //임시 배열
 
-    public static void mergeSort(int[] arr, int left, int right) {
-        if(left < right) {
-            int i;
-            int center = (left + right) / 2;      //
-            int p = 0;                            //임시 배열 인덱스
-            int j = 0;
-            int k = left;                        //원본 배열 인덱스
+    private static void mergeSort(int [] arr){
+        int [] tmp = new int[arr.length]; // 임시 저장소
+        mergeSort(arr, tmp, 0, arr.length-1);
 
-            mergeSort(arr, left, center);        //배열 앞부분 병합정렬
-            mergeSort(arr, center+1, right);    //배열 뒷부분 병합정렬
+    }
 
-            for(i = left; i <= center; i++) {    //임시 배열에 배열 앞부분을 넣어줌.
-                buff[p++] = arr[i];
-            }
+    private static void mergeSort(int[] arr, int[] tmp, int start, int end){
+        if(start < end) { // 시작 인덱스가 끝 인덱스 보다 작은 동안만 재귀 호출
+            int mid = (start + end) / 2;
+            mergeSort(arr, tmp, start, mid);
+            mergeSort(arr, tmp, mid + 1, end);
+            merge(arr, tmp, start, mid, end);
 
-            while(i <= right && j < p) {        //원본 배열의 뒷부분, 추가 배열의 앞부분의 원소끼리 비교한다.
-                arr[k++] = buff[j] <= arr[i] ? buff[j++] : arr[i++];
-            }
-
-            while(j < p) {                        //임시 배열의 앞부분이 모두 커서 선택이 안됐다면 원본배열 뒷부분에 붙혀준다.
-                arr[k++] = buff[j++];            //여기서 왜 i < right는 비교를 안할까? -> 비교를 하면서 선택을 못받은 잠재적 i값(큰 값)들은 원본배열 뒷부분에 있기 때문이다.
-            }
         }
     }
+
+    private static void merge(int [] arr, int [] tmp, int start, int mid, int end){
+        for(int i = start; i <= end; i++){
+            tmp[i] = arr[i];// 범위까지 배열 복사
+        }
+        int part1 = start; // 파트 분배
+        int part2 = mid + 1;
+        int index = start; // 진짜 배열 인덱스 지정
+
+        while (part1 <= mid && part2 <= end) {
+            if(tmp[part1] <= tmp[part2]){
+                arr[index] = tmp[part1];
+                part1++;
+            }else {
+                arr[index] = tmp[part2];
+                part2 ++;
+            }
+            index ++;
+        }
+
+        for(int i=0; i<= mid - part1; i++) {
+            arr[index + i] = tmp[part1 + i];
+        }
+
+    }
+
 
     public static void main(String[] args) {
-        int[] arr = {3, 4, 2, 1, 6, 8, 9, 5};
-        int N = arr.length;
-        buff = new int[N];
 
-        mergeSort(arr, 0, N-1); // 0부터 배열의 인덱스 만큼 돌려라
+        int [] arr = {3, 6, 2, 1, 9, 7, 4, 5};
+        mergeSort(arr);
 
-        for(int a : arr) {
-            System.out.print(a+" ");
+        System.out.println("====================병합정렬 후====================");
+
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
-
     }
+
+
+
 }
