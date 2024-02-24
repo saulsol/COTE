d1cd5f783063302ed2f08ef52ebf7081d5b286a4
saul <dlathf3210@naver.com>
2024-02-24 13:19:46 +0900

    02/24

---
+++ src/Easy_2/문자열/P_2.java
@@ -0,0 +1,29 @@
+package Easy_2.문자열;
+
+// 대문자와 소문자가 존재하는 문자열을 같이 받아 대문자는 소문자로, 소문자는 대문자로 변환하여 출력하는 프로그램 작
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P_2 {
+    public static void solution(String str){
+        char [] cArray = str.toCharArray();
+        String answer = "";
+        for (char c : cArray) {
+            if(Character.isLowerCase(c)){
+                answer += Character.toUpperCase(c);
+            }else{
+                answer += Character.toLowerCase(c);
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        solution(bufferedReader.readLine());
+
+    }
+}
