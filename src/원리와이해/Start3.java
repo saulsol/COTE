e92042c55dfd4003959b56deea3aa2d7d9123cbe
saul <dlathf3210@naver.com>
2025-01-07 20:02:55 +0900

    0107

---
+++ src/원리와이해/Start3.java
@@ -0,0 +1,19 @@
+package 원리와이해;
+
+public class Start3 {
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        int k = 20;
+
+        if(i == 10){
+            int m = k + 5;
+        }else{
+            int p = k + 10;
+            k = p;
+        }
+
+    }
+
+}
---
+++ src/원리와이해/Start4.java
@@ -0,0 +1,19 @@
+package 원리와이해;
+
+public class Start4 {
+    public static void main(String[] args) {
+
+        int k = 5;
+        int m;
+
+        m = square(k);
+    }
+
+    private static int square(int k){
+        int result;
+        k = 25;
+        result = k;
+        return result;
+    }
+
+}
