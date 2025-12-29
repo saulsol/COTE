7214fcb4b31b79e044131325bb917507b0869bbb
saul <dlathf3210@naver.com>
2025-12-29 12:47:53 +0900

    251229

--- src/forTest/HashMapPr.java
+++ src/forTest/HashMapPr.java
@@ -1,2 +1,15 @@
-package forTest;public class HashMapPr {
+package forTest;
+
+import java.util.HashMap;
+
+public class HashMapPr {
+    public static void main(String[] args) {
+
+        HashMap <String, Integer> nameList = new HashMap<>();
+        nameList.put("James", 10);
+        nameList.put("Garner", 20);
+
+        System.out.println(nameList.get("James"));
+
+    }
 }
